fd86d02659615ddb9d3bdf1833d5d86d46bcf530
Alberto Schiabel <skiabo97@gmail.com>
2021-10-29 12:01:03 +0200

    chore: updated big.sh slurm script

--- big.sh
+++ big.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=8
 #SBATCH --mem=32GB
 #SBATCH --time=72:00:00
 #SBATCH --error=stderr.big.txt
@@ -15,8 +15,8 @@ ml Python/3.8.6-GCCcore-10.2.0
 # generate samples
 echo "generate samples"
 python -u -m python.mcmc_sample -m \
-	obj=demo_monotone \
-	sample_size=huge \
+	obj=demo_monotone,delta_5 \
+	sample_size=small,middle,big,huge \
 	sampler=lovasz_projection
 
 # collect metrics
